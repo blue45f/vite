26942fcc1dbad2d70b72ffa49bc77f18f9bf224b
hjunkim <hjunkim@woowahan.com>
2021-06-08 21:40:48 +0900

    [init] 초기화

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
+  ignorePatterns: ['node_modules/'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
